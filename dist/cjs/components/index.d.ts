bbbf1a527436bcec7c210a7dbd7040b623a35001
Rebel-Ovi <ovidiu.deliu@rebeldot.com>
2022-08-08 01:54:45 +0300

    Removed dist from the repository as it is auto-generated when users install the library

--- dist/cjs/components/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { default as RebelButton } from "./RebelButton";
--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-declare type RebelButtonProps = {
-    text?: string;
-    onClick?: () => void;
-};
-declare function RebelButton({ text, onClick }: RebelButtonProps): JSX.Element;
-
-export { RebelButton };
